b1241426ef17e263bb3ae7891d193d43075ce4ef
Luisa-T <35731724+Luisa-T@users.noreply.github.com>
2018-02-25 11:00:37 +0000

    lecture

---
+++ range.py
@@ -0,0 +1,12 @@
+# Luisa Timothy 24-02-2018
+# functions are named blocks of code
+# range()
+
+for i in range(5): # for loop
+  print(i) # creates a list of the data that is specified
+
+
+a = ['Mary', 'had', 'a', 'little', 'lamb']
+
+for i in range(len(a)):
+  print(i, a[i])
