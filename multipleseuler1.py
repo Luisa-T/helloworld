aed4fb62f9c1c8571eef20512159e8dab2aedb99
Luisa-T <35731724+Luisa-T@users.noreply.github.com>
2018-02-25 11:01:12 +0000

    euler1

---
+++ multipleseuler1.py
@@ -0,0 +1,14 @@
+# Luisa Timothy 25-02-2018
+# project Euler problem 1
+
+sum = 0
+i = 1
+
+while i < 1000:
+  if i % 3 == 0:
+    sum = sum + i
+  elif i % 5 == 0:
+    sum = sum + i
+  i = i + 1
+
+print("Sum of multiples of 3 and 5, less than 1000", sum)
