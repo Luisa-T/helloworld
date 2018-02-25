ed646f16ee6b5fa64cdd39013546f1284b7f4c05
Luisa-T <35731724+Luisa-T@users.noreply.github.com>
2018-02-25 10:47:04 +0000

    following lectures. git not working for me so I need to do this manually

---
+++ Tuesday.py
@@ -0,0 +1,9 @@
+# Luisa timothy 25-02-2018
+# is it Tuesday?
+
+import datetime
+
+if datetime.datetime.today().weekday() == 1:
+  print("Yay it is Tuesday")
+else:
+  print("I am sorry it's not Tuesday")
